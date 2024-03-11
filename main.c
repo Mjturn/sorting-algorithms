a4e901e769ca19984c07502ac67ab170e5214906
Mjturn <Mjturn@outlook.com>
2024-03-11 19:56:23 -0400

    Implement selection sort algorithm

--- main.c
+++ main.c
@@ -28,38 +28,87 @@ void insertion_sort(int array[], const int SIZE) {
     }
 }
 
+void selection_sort(int array[], const int SIZE) {
+    for(int i = 0; i < SIZE - 1; i++) {
+        int minimum = i;
+        for(int j = i + 1; j < SIZE; j++) {
+            if(array[minimum] > array[j]) {
+                minimum = j;
+            }
+        }
+
+        int temp = array[i];
+        array[i] = array[minimum];
+        array[minimum] = temp;
+    }
+}
+
 int main() {
     const int SIZE = 5;
     const int MAX_VALUE = 100 + 1;
     
     srand(time(0));
-    int array[SIZE];
+    int bubble_sort_array[SIZE];
+    int insertion_sort_array[SIZE];
+    int selection_sort_array[SIZE];
 
     for(int i = 0; i < SIZE; i++) {
         int value = (rand() % MAX_VALUE);
-        array[i] = value;
+        bubble_sort_array[i] = value;
+    }
+    
+    for(int i = 0; i < SIZE; i++) {
+        int value = (rand() % MAX_VALUE);
+        insertion_sort_array[i] = value;
+    }
+    
+    for(int i = 0; i < SIZE; i++) {
+        int value = (rand() % MAX_VALUE);
+        selection_sort_array[i] = value;
     }
 
     printf("Original Array:\n");
     for(int i = 0; i < SIZE; i++) {
-        printf("%d\n", array[i]);
+        printf("%d\n", bubble_sort_array[i]);
     }
 
-    bubble_sort(array, SIZE);
+    selection_sort(bubble_sort_array, SIZE);
     printf("\n");
 
     printf("Bubble Sorted Array:\n");
     for(int i = 0; i < SIZE; i++) {
-        printf("%d\n", array[i]);
+        printf("%d\n", bubble_sort_array[i]);
+    }
+
+    printf("\n");
+    
+    printf("Original Array:\n");
+    for(int i = 0; i < SIZE; i++) {
+        printf("%d\n", insertion_sort_array[i]);
     }
     
-    insertion_sort(array, SIZE);
+    insertion_sort(insertion_sort_array, SIZE);
     printf("\n");
 
     printf("Insertion Sorted Array:\n");
     for(int i = 0; i < SIZE; i++) {
-        printf("%d\n", array[i]);
+        printf("%d\n", insertion_sort_array[i]);
     }
+    
+    printf("\n");
+    
+    printf("Original Array:\n");
+    for(int i = 0; i < SIZE; i++) {
+        printf("%d\n", selection_sort_array[i]);
+    }
+    
+    selection_sort(selection_sort_array, SIZE);
+    printf("\n");
 
+    printf("Selection Sorted Array:\n");
+    for(int i = 0; i < SIZE; i++) {
+        printf("%d\n", selection_sort_array[i]);
+    }
+    
     return 0;
 }
